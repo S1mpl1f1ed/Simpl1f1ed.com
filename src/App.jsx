6f4559bf31e581176211f149fce25ac267733301
MScheiterle <scheiterlem@gmail.com>
2023-07-25 15:18:40 -0400

    Undo previous, fix errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Navbar from "./Components/Layout/Navbar/Navbar";
-import NoPage from "./Components/Layout/NoPage/NoPage";
-import Footer from "./Components/Layout/Footer/Footer";
 import Home from "./Components/Layout/Home/Home";
-import Login from "./Components/Accounts/Login";
-import Logout from "./Components/Accounts/Logout";
-import Register from "./Components/Accounts/Register";
-import Reset from "./Components/Accounts/Reset";
-import ViewProfile from "./Components/Accounts/Profiles/ViewProfile";
-import MyProfile from "./Components/Accounts/Profiles/MyProfile";
 
 import "./index.scss";
 
